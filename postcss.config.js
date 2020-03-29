f7ec827aba253055fe848cac0663216ba1584328
David Behar <david@behar.io>
2020-03-29 16:27:00 -0600

    selectors

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')({
-      grid: true
-    })
+    // require('autoprefixer')({
+    //   grid: true
+    // }),
+
+    require('postcss-custom-media')({
+      preserve: false
+    }),
+    require('postcss-preset-env')({
+      stage: 1,
+      autoprefixer: {
+        grid: true
+      },
+      preserve: false
+    }),
+    require('postcss-color-hwb'),
+    require('postcss-apply')
+
   ]
 }
